ab4f4cb039aaab84726b5da619856d3b62ee6e3b
juanc <juan.lopez15@utp.edu.co>
2023-08-07 16:54:33 -0500

    added WebSpeechApi

--- src/main.js
+++ src/main.js
@@ -1,10 +1,68 @@
-import viewHandler from "./handler.js";
+import viewHandler from "./proxy/handler.js";
+import RecordToFile from "./plugin/RecordToFile.js";
+import FileToText from "./plugin/FileToText.js";
+import AudioToText from "./AudioToText.js";
+import { viewProxy } from "./proxy/proxy.js";
+
 const buttonPlus = document.querySelector(".plus");
 const buttonLess = document.querySelector(".less");
 const buttonDark = document.querySelector(".buttonDark");
-const text = document.querySelector(".textBox");
-const body = document.querySelector("body");
+const buttonRecord = document.querySelector(".buttonMicrophone");
+const buttonSend = document.querySelector(".buttonSend");
+const buttonPaste = document.querySelector(".buttonPaste");
+const buttonExport = document.querySelector(".buttonExport");
+const rawText = document.querySelector(".rawText p");
+const editableText = document.querySelector(".editableText");
+const content = document.querySelector(".content");
 
-buttonPlus.addEventListener("click",viewHandler, false)
-buttonLess.addEventListener("click",viewHandler, false)
-buttonDark.addEventListener("click",viewHandler, false)
+const proxy = viewProxy();
+const recorder = new AudioToText({
+  writerPlugin: FileToText,
+  recordPlugin: RecordToFile,
+  keywords: Object.keys(proxy),
+});
+buttonRecord.addEventListener(
+  "mousedown",
+  () => {
+    recorder.record();
+  },
+  false
+);
+
+let transcription;
+buttonPaste.addEventListener(
+  "mouseup",
+  () => {
+    transcription = recorder.transcription;
+    rawText.textContent += transcription;
+  },
+  false
+);
+
+buttonSend.addEventListener(
+  "click",
+  () => {
+    editableText.value = rawText.textContent;
+    rawText.textContent = "";
+  },
+  false
+);
+buttonExport.addEventListener(
+  "click",
+  () => {
+    // crea un nuevo div
+    // y añade contenido
+    console.log(editableText.value);
+    let newDiv = document.createElement("div");
+    let newContent = document.createTextNode(editableText.value);
+    newDiv.appendChild(newContent); //añade texto al div creado.
+    newDiv.style.backgroundColor = "aqua";
+    // añade el elemento creado y su contenido al DOM
+    content.appendChild(newDiv);
+    editableText.value = "";
+  },
+  false
+);
+buttonPlus.addEventListener("click", viewHandler, false);
+buttonLess.addEventListener("click", viewHandler, false);
+buttonDark.addEventListener("click", viewHandler, false);
--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -1,21 +1,28 @@
 const buttonPlus = document.querySelector(".plus");
 const buttonLess = document.querySelector(".less");
 const buttonDark = document.querySelector(".buttonDark");
-const text = document.querySelector(".textBox");
 const body = document.querySelector("body");
+const rawText = document.querySelector(".rawText p");
+const editableText = document.querySelector(".editableText");
 
 const setDark = () => {
+    rawText.style.backgroundColor = "black";
+    editableText.style.backgroundColor = "black";
+    editableText.style.color = "white";
     body.style.color = "white";
     body.style.backgroundColor = "black";
 }
 const setLight = () => {
+    rawText.style.backgroundColor = "white";    
+    editableText.style.backgroundColor = "white";
+    editableText.style.color = "black";
     body.style.color = "black";
     body.style.backgroundColor = "#E6D0DE";
 }
 const viewActions = {
     darkMode: () => (body.style.color === "white"? setLight() : setDark()),
-    lessSize: () => text.style.fontSize = "large",
-    plusSize: () => text.style.fontSize = "xx-large",
+    lessSize: () => body.style.fontSize = "large",
+    plusSize: () => body.style.fontSize = "xx-large",
 }
 
 const viewHandler = {
@@ -25,7 +32,7 @@ const viewHandler = {
         }
         if (value === "+"){
             value = "plusSize"
-        } 
+        }
         obj[value]()
     
         return true;
